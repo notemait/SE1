b64f3932a14ec055a17d1e54fcf60137a55fa46a
The lord <lebedko020702@gmail.com>
2023-05-04 16:48:55 +0500

    изначальная программа без изменений и без меня автора

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
-}
+ int main()
+  {
+       int a, b;
+        cout << "Enter A and B: ";
+        cin >> a >> b;
+        cout << "A + B = " << a + b << '\n'
+            << "A - B = " << a - b << '\n'
+            << "A * B = " << a * b << '\n';
+            return 0;
+   }
